b413b5322bad26861ee34d3d13dee09bf0144bf4
aalmiray <aalmiray@gmail.com>
2017-10-18 12:43:46 +0200

    Add JUnit4 parameterized tests

---
+++ hello-pmvc/src/test/java/org/kordamp/javatrove/hello/pmvc/service/HelloServiceParameterizedTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016-2017 Andres Almiray
+ *
+ * This file is part of Java Trove Examples
+ *
+ * Java Trove Examples is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Trove Examples is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Java Trove Examples. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.kordamp.javatrove.hello.pmvc.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * @author Andres Almiray
+ */
+@RunWith(Parameterized.class)
+public class HelloServiceParameterizedTest {
+    @Parameterized.Parameters(name = "Invoking HelloService.sayHello(''{0}'') yields ''{1}''")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+            new String[]{"", "Howdy stranger!"},
+            new String[]{"Test", "Hello Test"}
+        );
+    }
+
+    private final String input;
+    private final String output;
+
+    public HelloServiceParameterizedTest(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Test
+    public void sayHello() {
+        // given:
+        HelloService service = new DefaultHelloService();
+        // expect:
+        assertThat(service.sayHello(input), equalTo(output));
+    }
+}
