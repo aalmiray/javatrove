fbed9375e7ae62bcc5fb54a8e4d1bf38d94d5ee4
aalmiray <aalmiray@gmail.com>
2017-11-27 15:02:22 -0600

    Use strict mocking & stubbing

--- hello-pmvc/src/test/groovy/org/kordamp/javatrove/hello/pmvc/controller/AppControllerSpec.groovy
+++ hello-pmvc/src/test/groovy/org/kordamp/javatrove/hello/pmvc/controller/AppControllerSpec.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2016-2017 Andres Almiray
+ *
+ * This file is part of Java Trove Examples
+ *
+ * Java Trove Examples is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Trove Examples is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Java Trove Examples. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.kordamp.javatrove.hello.pmvc.controller
 
 import org.kordamp.javatrove.hello.pmvc.AppModule
@@ -15,14 +33,13 @@ import javax.inject.Inject
  */
 @UseModules(TestModule)
 class AppControllerSpec extends Specification {
-
     @Inject private AppController controller
     @Inject private AppModel model
     @Inject private HelloService service
 
     def happyPath() {
         given: "The HelloService mock is configured to return 'Hello \$input'"
-        service.sayHello('Test') >> 'Hello Test'
+        1 * service.sayHello('Test') >> 'Hello Test'
 
         when: "The input is set to 'Test'"
         model.input = 'Test'
@@ -31,7 +48,7 @@ class AppControllerSpec extends Specification {
         controller.sayHello()
 
         then: "The output should be 'Hello Test'"
-        model.output == ' Hello Test'
+        model.output == 'Hello Test'
     }
 
     static class TestModule extends AppModule {
